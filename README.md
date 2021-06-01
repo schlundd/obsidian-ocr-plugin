2ba5ae680b1dde9b6ec7959c1955e78ba31cfd7e
Dominik Schlund <dominik.schlund@eddyson.de>
2021-06-01 09:29:55 +0200

    first version

--- README.md
+++ README.md
@@ -1,57 +1,33 @@
-## Obsidian Sample Plugin
+## Obsidian OCR Plugin
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+![](demo.gif)
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+With this plugin you can extract the text of image attachments. It will create an annotation document next to the image document. This will contain a link to the image and the extracted text.
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+If you search for text, that is part of the image, it will yield the annotation document.
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Changes the default font color to red using `styles.css`.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+**Note**: This is an early version, but it works. It is not in the official list of plugins, so you need to [install it manually](#Manually-installing-the-plugin).
 
-### First time developing plugins?
+**Note**: The process of extracting the text from the image is not done locally. The plugin uses an online API hosted at https://ocr.taskbone.com.
 
-Quick starting guide for new plugin devs:
+The image data is stored as long as neccessary for the text extraction and deleted immediately after that.
 
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+However, if this is a dealbreaker for you, see [Alternatives](#Alternatives).
 
-### Releasing new releases
+## Manually installing the plugin
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments.
-- Publish the release.
+- Clone this repo
+- npm i or yarn to install dependencies
+- npm run build
+- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/taskbone-ocr-plugin/`.
+## Alternatives
+### Local Tesseract installation
+See https://forum.obsidian.md/t/basic-ocr-in-obsidian/18087
 
-### Adding your plugin to the community plugin list
+Advantages:
+* runs locally on your machine
 
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
-
-### How to use
-
-- Clone this repo.
-- `npm i` or `yarn` to install dependencies
-- `npm run dev` to start compilation in watch mode.
-
-### Manually installing the plugin
-
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-### API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+Disadvantages:
+* runs locally on your machine
+* probably slower
+* probably worse results
--- main.ts
+++ main.ts
@@ -1,60 +1,44 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Plugin, PluginSettingTab, Setting, SuggestModal } from 'obsidian';
 
-interface MyPluginSettings {
-	mySetting: string;
+interface TaskboneOCRPluginSettings {
+	token: string
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: TaskboneOCRPluginSettings = {
+	// for testing purposes
+	// will be removed
+	// token valid until Monday, June 14, 2021 4:36:36 AM
+	token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ3d3cudGFza2JvbmUuY29tIiwiYXVkIjoib2NyLnRhc2tib25lLmNvbSIsInN1YiI6InRlc3QiLCJleHAiOjE2MjM2NDUzOTZ9.tOWQkBRhPsfYHCdK5xDTH50YL27zo2BZdJwnk5e0CM4'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const BASE_URL = "https://ocr.taskbone.com"
+
+export default class TaskboneOCRPlugin extends Plugin {
+	settings: TaskboneOCRPluginSettings;
 
 	async onload() {
-		console.log('loading plugin');
+		console.log('Taskbone OCR: start loading plugin');
 
 		await this.loadSettings();
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
-
-		this.addStatusBarItem().setText('Status Bar Text');
-
 		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
+			id: 'open-image-select-modal',
+			name: 'Create annotation page for image',
+			callback: () => {
+				const images = this.getNotAnnotatedImagePaths()
+				if (images.length > 0) {
+					new FileSelectorModal(this.app, this).open();
+				} else {
+					new ErrorModal(this.app, "All supported image files already have an annotation file.").open();
 				}
-				return false;
 			}
 		});
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new TaskboneOCRSettingTab(this.app, this));
 	}
 
 	onunload() {
-		console.log('unloading plugin');
+		console.log('Taskbone OCR: unloading plugin');
 	}
 
 	async loadSettings() {
@@ -64,48 +48,137 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async getTextForFile(path: string): Promise<string> {
+		const vault = this.app.vault;
+		const file = vault.getFiles().filter((file) => {
+			return file.path == path
+		})[0]
+		const fileBuffer = await this.app.vault.readBinary(file);
+		const formData = new FormData();
+		formData.append('image', new Blob([fileBuffer]))
+		const response = await fetch(BASE_URL + '/get-text', {
+			headers: {
+				'Authorization': 'Bearer ' + this.settings.token
+			},
+			method: "post",
+			body: formData
+		});
+		if (response.status == 200) {
+			const jsonResponse = await response.json();
+			const text = jsonResponse?.text
+			return text || ''
+		} else {
+			const errorText = `Could not read Text from ${path}:<br/> Error: ${response.status}`
+			new ErrorModal(this.app, errorText).open();
+		}
+	}
+
+	async createAnnotationFileForFile(path: string): Promise<void> {
+		const imageText = await this.getTextForFile(path)
+		if (!imageText) return
+		if (imageText.length == 0) {
+			new ErrorModal(this.app, `No text found in ${path}`).open()
+		} else {
+			const annotationFileContent = `![[${path}]]\n\n${imageText}`
+			const annotationFilePath = path + '.annotations.md'
+			this.app.vault.create(annotationFilePath, annotationFileContent)
+		}
+	}
+
+	getNotAnnotatedImagePaths(): string[] {
+		const files = this.app.vault.getFiles()
+		const markdownFilePaths = this.app.vault.getMarkdownFiles().map((file) => {
+			return file.path
+		});
+		const images = files.filter((file) => {
+			const isImage = ['png', 'jpg', 'jpeg'].includes(file.extension)
+			if (!isImage) return false
+			const annotationFilePath = file.path + '.annotations.md'
+			const annotationFileAlreadyExist = markdownFilePaths.contains(annotationFilePath);
+			return !annotationFileAlreadyExist
+		})
+		const imagePaths = images.map((file) => {
+			return file.path
+		})
+		return imagePaths;
+	}
+
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class FileSelectorModal extends SuggestModal<string> {
+	plugin: TaskboneOCRPlugin
+
+	constructor(app: App, plugin: TaskboneOCRPlugin) {
 		super(app);
+		this.app = app;
+		this.plugin = plugin
+	}
+
+	getSuggestions(query: string): string[] {
+
+		const imagePaths = this.plugin.getNotAnnotatedImagePaths()
+		if (query.length == 0) {
+			return imagePaths
+		}
+		const filteredImageNames = imagePaths.filter((path, index, paths) => {
+			const match = path.contains(query)
+			return path.contains(query)
+		})
+		return filteredImageNames
+	}
+
+	renderSuggestion(value: string, el: HTMLElement): void {
+		el.setText(value);
+	}
+
+	async onChooseSuggestion(item: string, evt: MouseEvent | KeyboardEvent): Promise<void> {
+		this.plugin.createAnnotationFileForFile(item);
+	}
+}
+
+class ErrorModal extends Modal {
+
+	message: string
+
+	constructor(app: App, message: string) {
+		super(app);
+		this.message = message
 	}
 
 	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
+		this.titleEl.setText("Taskbone OCR Error")
+		this.contentEl.innerHTML = this.message
 	}
 
 	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
+		this.containerEl.empty()
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class TaskboneOCRSettingTab extends PluginSettingTab {
+	plugin: TaskboneOCRPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: TaskboneOCRPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		let {containerEl} = this;
+		let { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Taskbone OCR Settings' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Authentication Token')
+			.setDesc('The token used to authenticate at the online OCR Service')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
+				.setPlaceholder('Enter your token')
+				.setValue(this.plugin.settings.token)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.token = value;
 					await this.plugin.saveSettings();
 				}));
 	}
